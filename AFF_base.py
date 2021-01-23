156540799514d1de9830ccab8dfa256fb01cadf3
bigmalion <77898722+bigmalion@users.noreply.github.com>
2021-01-23 21:31:20 +0100

    Update AFF_base.py

--- AFF_base.py
+++ AFF_base.py
@@ -22,7 +22,7 @@ def AFF_Rectangle(canvas,x:int, y:int, longueur:int, largeur:int, **arguments)->
     - longueur la longueur du rectangle voulut
     - arguments le dictionnaire contenant toutes les informations supplémentaires pour le rectangle
     
-    Retourne : 1 (si tout ces bien passé)
+    Retourne : l'adresse memoire du rectangle
     """
     return canvas.create_rectangle(AFF_TransCoord(x, y, longueur, largeur, **arguments))
 
@@ -37,31 +37,7 @@ def AFF_Cercle(canvas,x:int, y:int, rayon:int, **arguments)-> list:
     - rayon le rayon du cercle
     - arguments le dictionnaire contenant toutes les informations supplémentaires pour le cercle
     
-    Retourne : 1 (si tout ces bien passé)
+    Retourne : l'adresse memoire du cercle
     """
     return canvas.create_oval(AFF_TransCoord(x, y, rayon*2, rayon*2, **arguments))
 
-def AFF_Image(canvas,x:int, y:int, longueur:int, largeur:int, fichier:str, **arguments)-> list:
-    """
-    Creer et retourne un rectangle
-    
-    Paramètre :
-    - canvas le canvas ou doit etre creer l'image
-    - x l'abcisse du centre de l'image voulut
-    - y l'ordonné du centre de l'image voulut
-    - largeur la largeur de l'image voulut
-    - longueur la longueur de l'image voulut
-    - arguments le dictionnaire contenant toutes les informations supplémentaires pour le rectangle
-    
-    Retourne : 1 (si tout ces bien passé)
-    """
-    
-    return canvas.create_rectangle(AFF_TransCoord(x, y, longueur, largeur, **arguments))
-
-
-fenetre = Tk()
-
-can = Canvas(fenetre)
-can.pack()
-
-AFF_Cercle(can, 100, 100, 5,fill='red')
